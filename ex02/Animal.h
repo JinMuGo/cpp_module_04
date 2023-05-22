2c7b04902d08fd53bf30ee78206ab03921a92e8f
jgo-42 <jingmuio@gmail.com>
2023-05-22 12:04:06 +0900

    :pencil2: Fix: a typo

--- ex02/Animal.h
+++ ex02/Animal.h
@@ -6,7 +6,7 @@
 /*   By: jgo <jgo@student.42seoul.fr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/19 14:26:46 by jgo               #+#    #+#             */
-/*   Updated: 2023/05/20 15:00:56 by jgo              ###   ########.fr       */
+/*   Updated: 2023/05/22 12:01:00 by jgo              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 #include <iostream>
 #include "const.h"
 
-#define ANI_DFLT_CTOR (GREEN " Animal: Default constructor called" RESET)
+#define ANI_DFLT_CTOR GREEN " Animal: Default constructor called" RESET
 #define ANI_CTR LIGHT_CYAN" Animal: constructor called" RESET
 #define ANI_CPY_CTOR CYAN" Animal: Copy constructor called" RESET
 #define ANI_CPY_ASGMT_OP_CALL DARK_CYAN" Animal: Copy assignment operator called" RESET
